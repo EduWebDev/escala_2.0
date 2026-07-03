b8f68e3003a08b45d4dcfa62181e69aa37a5d83a
unknown <eduardo81contact@gmail.com>
2026-07-03 20:52:40 -0300

    Ajuste de permissao e base url do Vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/escala_2.0/',
   build: {
     sourcemap: false // Remove os mapas de código do build final
   }
